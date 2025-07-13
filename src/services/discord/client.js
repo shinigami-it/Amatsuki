b190bece6a472ffb65b04f1181a00ea671f919c7
DasHund007 <dashund007@gmail.com>
2025-07-13 17:58:58 +0300

    initial commit

--- src/services/discord/client.js
+++ src/services/discord/client.js
@@ -1,6 +1,6 @@
 const chalkImport = require("chalk");
 const chalk = chalkImport.default || chalkImport;
-const { Client, GatewayIntentBits, GatewayIntents } = require("discord.js");
+const { Client, GatewayIntentBits, GatewayIntents, ActivityType } = require("discord.js");
 
 const client = new Client({
 	intents: [
@@ -20,6 +20,11 @@ client.once("ready", () => {
 	console.log(
 		chalk.magenta.bold.underline(`Logged in as ${client.user.tag} ✅`)
 	);
+
+	client.user.setActivity('Amatsuki - API', {
+	    type: ActivityType.Streaming,
+	    url: 'https://twitch.tv/dashund007'
+	});
 });
 
 module.exports = client;
